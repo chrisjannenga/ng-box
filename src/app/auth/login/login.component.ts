1913352171cdd75c6673672fea1479234e519a7e
Chris Jannenga <cjannenga312@gmail.com>
2020-05-26 22:37:19 -0700

    remember me cookie

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -11,6 +11,9 @@ export class LoginComponent implements OnInit {
   validateForm!: FormGroup;
 
   submitForm(): void {
+    if (this.validateForm.controls.remember.value) {
+      localStorage.setItem('rememberMeStatus', this.validateForm.controls.remember.value);
+    }
     // tslint:disable-next-line:forin
     for (const i in this.validateForm.controls) {
       this.validateForm.controls[i].markAsDirty();
